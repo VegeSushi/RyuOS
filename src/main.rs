3d63fb24ca8d58bad073ed8ddcd70bef3c813a52
VegeSushi <vegesushi19@gmail.com>
2026-02-20 17:22:55 +0100

    Improved shell

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@
 use panic_halt as _;
 use rp2040_hal as hal;
 use hal::pac;
-
-// USB Serial Trait Imports
 use usb_device::prelude::*;
 use usbd_serial::SerialPort;
+use core::str;
+use rp2040_hal::rom_data;
 
 #[link_section = ".boot2"]
 #[used]
@@ -29,8 +29,6 @@ fn main() -> ! {
     )
     .unwrap();
 
-    // 1. Setup the USB Driver
-    // Note the change from pac.USB to USBCTRL_REGS and USBCTRL_DPRAM
     let usb_bus = usb_device::bus::UsbBusAllocator::new(
         hal::usb::UsbBus::new(
             pac.USBCTRL_REGS,
@@ -41,34 +39,128 @@ fn main() -> ! {
         )
     );
 
-    // 2. Setup the Serial Port (CDC)
     let mut serial = SerialPort::new(&usb_bus);
 
-    // 3. Setup the USB Device
-    // In usb-device 0.3.x, manufacturer and product are set via strings in the builder
     let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
-        .strings(&[StringDescriptors::default()
-            .manufacturer("RyuOS")
-            .product("Ryu-Native-Serial")
-            .serial_number("001")])
+        .strings(&[
+            StringDescriptors::default()
+                .manufacturer("RyuOS")
+                .product("Ryu-Native-Serial")
+                .serial_number("001")
+        ])
         .unwrap()
-        .device_class(2) 
+        .device_class(2) // Communications Device Class (CDC)
         .build();
 
-    let mut timer = 0u32;
+    // --- Command Parser State ---
+    let mut input_buf = [0u8; 64];
+    let mut input_pos = 0usize;
 
     loop {
-        // 4. Poll the USB device
         if usb_dev.poll(&mut [&mut serial]) {
-            let mut buf = [0u8; 64];
-            let _ = serial.read(&mut buf);
-        }
+            let mut read_buf = [0u8; 16];
+            match serial.read(&mut read_buf) {
+                Ok(count) if count > 0 => {
+                    for i in 0..count {
+                        let c = read_buf[i];
 
-        // 5. Non-blocking delay
-        timer += 1;
-        if timer >= 1_000_000 { 
-            let _ = serial.write(b"RyuOS Heartbeat over Native USB\r\n");
-            timer = 0;
+                        match c {
+                            // 1. Handle Backspace (ASCII 8) or Delete (ASCII 127)
+                            8 | 127 => {
+                                if input_pos > 0 {
+                                    input_pos -= 1;
+                                    // Visual erase: Backspace, Space, Backspace
+                                    let _ = serial.write(b"\x08 \x08");
+                                }
+                            }
+                            // 2. Handle Newline (Enter)
+                            b'\r' | b'\n' => {
+                                let _ = serial.write(b"\r\n");
+                                if input_pos > 0 {
+                                    if let Ok(command_line) = str::from_utf8(&input_buf[..input_pos]) {
+                                        handle_command(&mut serial, command_line);
+                                    }
+                                    input_pos = 0; // Reset buffer
+                                }
+                                let _ = serial.write(b"> "); // New prompt
+                            }
+                            // 3. Handle Regular Characters
+                            _ => {
+                                if input_pos < input_buf.len() {
+                                    input_buf[input_pos] = c;
+                                    input_pos += 1;
+                                    let _ = serial.write(&[c]); // Echo character
+                                }
+                            }
+                        }
+                    }
+                }
+                _ => {}
+            }
+        }
+    }
+}
+
+/// Simple command parser that splits by whitespace
+fn handle_command(serial: &mut SerialPort<hal::usb::UsbBus>, line: &str) {
+    let mut args = line.split_whitespace();
+    
+    if let Some(cmd) = args.next() {
+        match cmd {
+            "ping" => {
+                let _ = serial.write(b"pong\r\n");
+            }
+            "echo" => {
+                for arg in args {
+                    let _ = serial.write(arg.as_bytes());
+                    let _ = serial.write(b" ");
+                }
+                let _ = serial.write(b"\r\n");
+            }
+            "about" => {
+                let _logo = r#"                                             
+                         @-                       
+                       **--%                      
+                 #******+--#                      
+              *@******@.--:            @::--. #   
+          .- ********@ @*%#.       .--------..    
+            --@*@  %*.---=:--+    - ---#@---      
+              -@@@---@--@:#       -:-**---.       
+              % .%----=-%         -@** :-         
+              @@     -@--*#=%.    ***-#           
+                  +#*-----***=.  #**              
+                 =***-----****=:***               
+                :@***---:-@****=**#               
+                -%**@-%--@-@**#=                  
+               @#@**%------=*****@                
+             .**-*****------**%*----              
+             ----=-***--*%---*@-----@             
+             -------#--@---+--------              
+              *---@---*####@#-----                
+                 #=---#*##=-++*@                  
+                  +@@     #--@                             
+
+                RyuOS v0.1.0
+                Built for RP2040 (Native USB)
+                        "#;
+                for line in _logo.lines() {
+                    let _ = serial.write(line.as_bytes());
+                    let _ = serial.write(b"\r\n");
+                }
+            }
+            "reboot" => {
+                let _ = serial.write(b"Rebooting...\r\n");
+                hal::pac::SCB::sys_reset();
+            }
+            "bootsel" => {
+                let _ = serial.write(b"Jumping to Bootloader...\r\n");
+                rom_data::reset_to_usb_boot(0, 0);
+            }
+            _ => {
+                let _ = serial.write(b"Unknown command: ");
+                let _ = serial.write(cmd.as_bytes());
+                let _ = serial.write(b"\r\n");
+            }
         }
     }
 }
